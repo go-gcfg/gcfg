04225cb99a72c7361c624c71632b8981617c64c0
speter <speter.go1@gmail.com>
2013-02-18 23:30:25 +0900

    improve tests

--- issues_test.go
+++ issues_test.go
@@ -1,34 +1,48 @@
 package gcfg
 
 import (
+	"math/big"
 	"strings"
 	"testing"
 )
 
 type Config1 struct {
 	Section struct {
-		Int int
+		Int    int
+		BigInt big.Int
 	}
 }
 
-var testsIssue1 = []string{
-	"[section]\nint=X",
-	"[section]\nint=",
-	"[section]\nint=1A",
+var testsIssue1 = []struct {
+	cfg      string
+	typename string
+}{
+	{"[section]\nint=X", "int"},
+	{"[section]\nint=", "int"},
+	{"[section]\nint=1A", "int"},
+	{"[section]\nbigint=X", "big.Int"},
+	{"[section]\nbigint=", "big.Int"},
+	{"[section]\nbigint=1A", "big.Int"},
 }
 
-// value parse error message shouldn't include reflect internals
+// Value parse error should:
+//  - include plain type name
+//  - not include reflect internals
 func TestIssue1(t *testing.T) {
 	for i, tt := range testsIssue1 {
 		var c Config1
-		err := ReadStringInto(&c, tt)
+		err := ReadStringInto(&c, tt.cfg)
 		switch {
 		case err == nil:
-			t.Errorf("%d: ok; wanted error", i)
+			t.Errorf("%d fail: got ok; wanted error", i)
+		case !strings.Contains(err.Error(), tt.typename):
+			t.Errorf("%d fail: error message doesn't contain type name %q: %v",
+				i, tt.typename, err)
 		case strings.Contains(err.Error(), "reflect"):
-			t.Errorf("%d: error message includes reflect internals: %v", i, err)
+			t.Errorf("%d fail: error message includes reflect internals: %v",
+				i, err)
 		default:
-			t.Logf("%d: %v", i, err)
+			t.Logf("%d pass: %v", i, err)
 		}
 	}
 }
